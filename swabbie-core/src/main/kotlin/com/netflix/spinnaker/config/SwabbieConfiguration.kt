69c5e64a7687151580ffc5d8bea4a98b39f32fde
jeyrs <jeyrschabu@gmail.com>
2018-01-14 17:15:44 -0800

    Initial Commit

---
+++ swabbie-core/src/main/kotlin/com/netflix/spinnaker/config/SwabbieConfiguration.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.config
+
+import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
+import com.fasterxml.jackson.databind.DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+
+@Configuration
+open class SwabbieConfiguration {
+  @Bean
+  @Primary
+  open fun objectMapper() = ObjectMapper().apply {
+    registerModule(KotlinModule())
+    registerModule(JavaTimeModule())
+    disable(FAIL_ON_UNKNOWN_PROPERTIES)
+    disable(READ_DATE_TIMESTAMPS_AS_NANOSECONDS)
+  }
+}

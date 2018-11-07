c4eeddcf59b0ed18e1a580070cff15684ed8bd7c
Emily Burns <emjeburns@gmail.com>
2018-11-07 12:57:13 -0800

    fix(discovery): make discovery activated work

--- swabbie-core/src/main/kotlin/com/netflix/spinnaker/swabbie/discovery/DiscoveryPollingConfiguration.kt
+++ swabbie-core/src/main/kotlin/com/netflix/spinnaker/swabbie/discovery/DiscoveryPollingConfiguration.kt
@@ -1,47 +1,22 @@
 package com.netflix.spinnaker.swabbie.discovery
 
-import com.netflix.appinfo.InstanceInfo
-import com.netflix.discovery.DiscoveryClient
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
-import org.springframework.context.ApplicationEventPublisher
-import org.springframework.context.ApplicationListener
+import com.netflix.discovery.EurekaClient
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.event.ContextRefreshedEvent
 import java.lang.management.ManagementFactory
+import java.util.*
 
 @Configuration
 open class DiscoveryPollingConfiguration {
 
-  @Configuration
-  @ConditionalOnMissingBean(DiscoveryClient::class)
-  open class NoDiscoveryConfiguration(
-    @Autowired var publisher: ApplicationEventPublisher
-  ) {
-
-    @Value("\${spring.application.name:swabbie}")
-    internal var appName: String? = null
-
-    @Bean
-    open fun discoveryStatusPoller(): ApplicationListener<ContextRefreshedEvent> {
-      return NoDiscoveryApplicationStatusPublisher(publisher)
-    }
-
-    @Bean
-    open fun currentInstanceId(): String {
-      return ManagementFactory.getRuntimeMXBean().name
-    }
-  }
-
-  @Configuration
-  @ConditionalOnBean(DiscoveryClient::class)
-  open class DiscoveryConfiguration {
-    @Bean
-    open fun currentInstanceId(instanceInfo: InstanceInfo): String {
-      return instanceInfo.instanceId
+  @Bean
+  open fun currentInstanceId(applicationContext: ConfigurableApplicationContext, eurekaClientProvider: Optional<EurekaClient>): String {
+    return if (eurekaClientProvider.isPresent) {
+      eurekaClientProvider.get().applicationInfoManager.info.instanceId
+    } else {
+      applicationContext.addApplicationListener(NoDiscoveryApplicationStatusPublisher(applicationContext))
+      ManagementFactory.getRuntimeMXBean().name
     }
   }
 }
